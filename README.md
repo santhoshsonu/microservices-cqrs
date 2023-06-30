44d1df62aad1766a642172cff804754425b1f448
Sonu <santhosheshwaramma@gmail.com>
2023-06-30 14:38:00 +0530

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# microservices-cqrs
+# Microservices - CQRS
 Microservices that are based on CQRS &amp; Event Sourcing. Powered by Spring Boot and Apache Kafka.
+
+## Architecture
+
+![Alt text](/assets/architecture.jpg "Architecture")
---
+++ account.query/src/main/java/com/microservices/cqrs/account/query/AccountQueryApplication.java
@@ -0,0 +1,11 @@
+package com.microservices.cqrs.account.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountQueryApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AccountQueryApplication.class);
+  }
+}
