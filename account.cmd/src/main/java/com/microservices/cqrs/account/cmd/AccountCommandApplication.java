fe30065d9bef3ac4b1ba7bd75b3c2491573eb3db
Sonu <santhosheshwaramma@gmail.com>
2023-06-30 19:50:10 +0530

    Add account.common and account.cmd modules. Refactored code, docker files

---
+++ account.cmd/src/main/java/com/microservices/cqrs/account/cmd/AccountCommandApplication.java
@@ -0,0 +1,11 @@
+package com.microservices.cqrs.account.cmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCommandApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AccountCommandApplication.class);
+  }
+}
